24e3190b65219dddb6b7ec5c04ec3b3da3e528c9
Akileshmuthu117 <akileshmuthu2000@gmail.com>
2025-06-28 10:49:45 +0530

    android build innitial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'in.akilesh.aadhi',
+  appName: 'Aadhi',
+  webDir: 'www'
+};
+
+export default config;
